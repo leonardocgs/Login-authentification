da091e6e933d098de228241215fbc8be565c12b2
leonardocgs <leonardocesargoes@gmail.com>
2022-07-29 08:09:24 -0300

    Add: User model

---
+++ src/api/models/User.ts
@@ -0,0 +1,11 @@
+class User {
+  private email: string;
+  private username: string;
+  private password: string;
+  constructor(email: string, username: string, password: string) {
+    this.email = email;
+    this.username = username;
+    this.password = password;
+  }
+}
+export { User };
